3d0762c56e0f2fca3aa8495dc57ed7e42fefec8e
Shampoo Shrek <luizgsiewerdt@gmail.com>
2024-04-06 12:31:31 -0300

    Shell and WeztermImage

--- wezTerm/.wezterm.lua
+++ wezTerm/.wezterm.lua
@@ -6,13 +6,25 @@ local config = {}
 -- config.default_prog = { "C:/Users/luizg/AppData/Local/Microsoft/WindowsApps/Microsoft.PowerShell_8wekyb3d8bbwe/pwsh.exe", "-l" }
 -- config.default_cwd = "C:/Users/luizg/Programming"
 
-config.color_scheme = "tlh"
+config.color_scheme = "tokyonight"
 
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
+config.font = wezterm.font("FiraCode Nerd Font", { weight = "Medium" })
 config.font_size = 9
 
-config.colors = { background = "#1C1C2C", cursor_bg = "#ffffff" }
-config.window_background_image = "./NixOS.jpg"
+config.colors = { background = "#141424", cursor_bg = "#ffffff" }
+-- config.window_background_image = "./NixOS.jpg"
+
+config.background = {
+  -- This is the deepest/back-most layer. It will be rendered first
+  {
+    source = {
+      File = './NixOS.jpg',
+    },
+    horizontal_align = "Center",
+    height = "Cover",
+    width = "Cover",
+  },
+}
 
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
